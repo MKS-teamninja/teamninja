53a1aab8c78ec62e4e8139b07aed5d6842f46cb7
MKS-students <mks.students@hackreactor.com>
2016-05-20 17:26:15 -0500

    Query ACTIVE.COM Campground API within worker script to collect campground and campsite data. This data is not yet saved anywhere.

--- worker/cggrabber.js
+++ worker/cggrabber.js
@@ -5,7 +5,46 @@
 //
 var cfg = require('../config');
 var db = require('../db/db');
-var request = require('request');
+var rp = require('request-promise');
+var xmlParse = require('xml2js').parseString;
 
-console.log("Hello worker!");
+var campgrounds = {
+  uri: 'http://api.amp.active.com/camping/campgrounds?pstate=TX&siteType=2001',
+  qs: {
+    api_key: cfg.getCgApiKey()
+  },
+  headers: {
+    'User-Agent': 'request-promise'
+  },
+  json: true
+};
 
+// TODO: fill in contractCode and parkId dynamically. Hardcoded for now.
+var campsites = {
+  uri: 'http://api.amp.active.com/camping/campsites?contractCode=CO&parkId=50032',
+  qs: {
+    api_key: cfg.getCgApiKey()
+  },
+  headers: {
+    'User-Agent': 'request-promise'
+  },
+  json: true
+};
+
+rp(campgrounds)
+  .then(function(res) {
+    xmlParse(JSON.stringify(res));
+    console.log("Successfully obtained campground info:", res);
+  })
+  .catch(function(err) {
+    console.log("Campground fetch error:", err);
+  });
+
+rp(campsites)
+  .then(function(res) {
+    xmlParse(JSON.stringify(res));
+    console.log("Successfully obtained campsite info:", res);
+  })
+  .catch(function(err) {
+    console.log("Campsite fetch error:", err);
+  });
