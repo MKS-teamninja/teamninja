66cdc2f45dd9ed84e2265f64a15617c01f6a3b6d
Madakkak <Madakkak@users.noreply.github.com>
2016-05-24 17:38:30 -0500

    Fix campsite queries so that all campgrounds can be successfullyl queried and saved into the database. Queries are throttled to 2 QPS using promise-throttle library (#36)

--- worker/cggrabber.js
+++ worker/cggrabber.js
@@ -1,40 +1,22 @@
 //
-// Campground grabber
+// cggrabber.js
 //
-// This worker queries the campground API once a day and regenerates the campground database.
+// Campground grabber queries the campground API
+// and regenerates the campground database.
 //
 var cfg = require('../config');
 var db = require('../db/db');
 var _ = require('underscore');
 var camp = require('./cghelpers');
+var PromiseThrottle = require('promise-throttle');
 
 //
-// Query info for Active's Campground Search API
+// PromisedThrottle will ensure we don't exceed 2 queries per second
 //
-var campgrounds = {
-  uri: 'http://api.amp.active.com/camping/campgrounds?pstate=TX',
-  qs: {
-    api_key: cfg.getCgApiKey()
-  },
-  headers: {
-    'User-Agent': 'request-promise'
-  },
-  json: true
-};
-
-//
-// Query info for Active's Campsite Search API
-//
-var campsite = {
-  uri: campsitesUrl,
-  qs: {
-    api_key: cfg.getCgApiKey()
-  },
-  headers: {
-    'User-Agent': 'request-promise'
-  },
-  json: true
-};
+var pthrottle = new PromiseThrottle({
+  requestsPerSecond: 2,
+  promiseImplementation: Promise
+});
 
 //
 // Base string used to query campsites of a specific campground
@@ -53,7 +35,7 @@ var worker = function() {
       return collectCampgrounds();
     })
     .then(function(cgs) {
-      // var testCgs = cgs.slice(0, 2);
+      // var testCgs = Array.prototype.slice(cgs, 0, 2);
       console.log("Collected campgrounds");
       return collectCampsites(cgs)
     })
@@ -71,9 +53,23 @@ var worker = function() {
 // inserts them into the campgrounds table
 //
 var collectCampgrounds = function() {
+  //
+  // Query info for Active's Campground Search API
+  //
+  var campgrounds = {
+    uri: 'http://api.amp.active.com/camping/campgrounds?pstate=TX',
+    qs: {
+      api_key: cfg.getCgApiKey()
+    },
+    headers: {
+      'User-Agent': 'request-promise'
+    },
+    json: true
+  };
+
   return camp.fetch(campgrounds)
     .then(function (cgs) {
-      // console.log("Cleaned results: ", cgs)
+      console.log("Cleaned results: ", cgs)
       //
       // Insert campground data into db
       //
@@ -83,18 +79,47 @@ var collectCampgrounds = function() {
 
 //
 // Fetches a scrubbed array of campsites for each campground and
-// inserts them into the campsites table
+// inserts them into the campsites table. Queries are throttled
+// to 2 per second as per Active API's QPS limit.
 //
 var collectCampsites = function(cgs) {
   return Promise.all(_.map(cgs, function(cg) {
-    var csQuery = _.extend({}, campsite);
-    csQuery.uri += 'contractCode=' +
-                   cg.contract_id +
-                   '&parkId=' +
-                   cg.facility_id;
-    return camp.fetch(csQuery, cg.facility_id)
-      .then(function (campsites) {
-        return db.insertEverything(campsites, 'campsites')
-      })
+    return pthrottle.add(queryCampsites.bind(this, cg))
+    .then(function() {
+      console.log("Campground queried");
+    })
   }))
+  .then(function() {
+    console.log("All campgrounds queried");
+  })
+}
+
+//
+// Query an individual campground for its campsites
+//
+var queryCampsites = function(cg) {
+  //
+  // Query info for Active's Campsite Search API
+  //
+  var campsite = {
+    uri: campsitesUrl,
+    qs: {
+      api_key: cfg.getCgApiKey()
+    },
+    headers: {
+      'User-Agent': 'request-promise'
+    },
+    json: true
+  };
+
+  var csQuery = _.extend({}, campsite);
+  csQuery.uri += 'contractCode=' +
+                 cg.contract_id +
+                 '&parkId=' +
+                 cg.facility_id;
+
+  return camp.fetch(csQuery, cg.facility_id)
+    .then(function (campsites) {
+      return db.insertEverything(campsites, 'campsites')
+    })
 }
