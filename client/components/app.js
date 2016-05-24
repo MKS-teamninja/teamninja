e74df123ad5acf9c402313d01c536c0cad733eaa
Jack Hall <jackjhall22@me.com>
2016-05-24 08:04:19 -0500

    Feat17 (#34)
    * move app.js into components directory
    change index.js in the server directory to accommodate app.js move
    comment out original app.js
    implement basic search box
    
    * implement constructor, -handeSubmit, _getInitialState, and _handleClick functions to the SearchBox component
    
    * Complete issue#4 (Collect campgrounds and campsites) (#33)
    
    * Switch out Postgres knex adapter for SQLite3. Add an empty db file. Create table schema upon starting connection to db file. Close the db connection when worker is finished querying.
    
    * Implement helper function to promisify the xml2js parser. Extend promise chain to parse the xml results from Campground API.
    
    * Further scrubbed results and built an array of campgrounds using only the info we need to store in the database.
    
    * Complete camp collector functionality. All campgrounds are now stored in the database. Additionally, each campground is queried for its own set of campsites, which are also collected and inserted into a separate table. Created a new cghelpers.js file to help modularize functions
    
    * implement class Search component, SearchList component, _addSearch and _getSearches methods
    
    * implement constructor, -handeSubmit, _getInitialState, and _handleClick functions to the SearchBox component
    
    * implement class Search component, SearchList component, _addSearch and _getSearches methods

--- client/components/app.js
+++ client/components/app.js
@@ -1,9 +1,85 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-class SearchBox extends React.Component {
+
+class Search extends React.Component {
+  
   render() {
-    return( <input type="text">Search Box</input> );
+    
+    return(
+      <div className="search">
+        <p className="comment-header">{this.props.author}</p>          
+      </div>
+    );
+  }
+}
+
+
+
+class SearchBox extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searches: [
+        { id: 1, author: 'Jack'},
+        { id: 2, author: 'Vidush'}
+      ]
+    };
+  }
+  render() {
+    const searches = this._getSearches();
+    return (
+      <div className='search-box'>
+        <SearchList addSearch={this._addSearch.bind(this)} />
+        <h3>Searches</h3>
+        <div className='seach-list'>
+          {searches}
+        </div>
+      </div>
+    );
+  }
+  _addSearch(searchAuthor) {
+    let search = {
+      id: this.state.searches.length + 1,
+      author: searchAuthor
+    };
+    this.setState({ 
+      searches: this.state.searches.concat([search])
+    });
+  }
+
+  _getSearches() {
+    return this.state.searches.map((search) => {
+      return (<Search
+                author={search.author}
+                key={search.id} />);
+    })
+  }
+}
+
+class SearchList extends React.Component {
+  render() {
+    return (
+      <form className='search-list' onSubmit={this._handleSubmit.bind(this)}>
+        <div className='search-list-fields'>
+          <input placeholder="Search Box" ref={(value) => this._author = value}/>
+        </div>
+        <div className='search-list-actions'>
+          <button type='submit'>
+            submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+  _handleSubmit(e) {
+    e.preventDefault();
+
+    this.props.addSearch(this._author.value);
+
+    this._author.value = '';
+    
   }
 }
 
@@ -13,61 +89,6 @@ ReactDOM.render(
 
 
 
-// ReactDOM.render(
-//   <h1>Hello, world!</h1>, // JSX
-//   // React.createElement("h1", null, "Hello, world!"),
-//   // m("h1", "Hello, world!"),
-//   document.getElementById('app')
-// );
 
 
-// var Waitlist = React.createClass({
-//   render: function () {
-//     return <div className="waitlist">
-//       {this.props.roster.map( person =>
-//         <div key={person.id} className="person">{ person.name }</div>
-//       )}
-//     </div>
-//   }
-// })
 
-// var WaitlistMgr = React.createClass({
-//   getInitialState: function () {
-//     return {
-//       people: [
-//         { id: 10, name: 'alice' },
-//         { id: 11, name: 'bob'}
-//       ],
-//       newPersonName: '[Type person name here]'
-//     }
-//   },
-
-//   setNewPersonName: function (e) {
-//     this.setState({ newPersonName: e.target.value })
-//   },
-
-//   add: function () {
-//     console.log("Adding new person:", this.state.newPersonName)
-//     this.state.people.push(
-//       { id: Math.round(Math.random() * 1000), name: this.state.newPersonName }
-//     )
-//   },
-
-//   render: function () {
-//     return <div className="waitlist-mgr">
-//       <Waitlist roster={this.state.people} />
-
-//       <input type="text"
-//         value={this.state.newPersonName}
-//         onChange={this.setNewPersonName} />
-
-//       <p>Add new person: {this.state.newPersonName}</p>
-//       <button onClick={this.add}>Add Person</button>
-//     </div>
-//   }
-// })
-
-// ReactDOM.render(
-//   <WaitlistMgr />,
-//   document.getElementById('app')
-// )
