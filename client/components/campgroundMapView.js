863ed16c25dd13cc4635d694ae2ae7bee22d17ba
Mark Boraski <SpartanSOS@users.noreply.github.com>
2016-05-25 15:07:01 -0500

    Google map basics (#42)
    * add static google map
    
    * Add google map icon

--- client/components/campgroundMapView.js
+++ client/components/campgroundMapView.js
@@ -1,1 +1,26 @@
 //campground map
+
+class CampgroundMap extends React.Component {
+	render(){
+		return (
+			<div className='campground-map'>
+			<!--CampgroundIcon Component-->
+			<div className='campground-icon'>
+			</div>
+			<!--CampgroundIcon End-->
+			</div>
+		)
+	}
+}
+
+
+
+
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 30.48276099999998, lng: -97.6564032},
+    scrollwheel: false,
+    zoom: 11
+  });
+}
