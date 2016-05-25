a8aee2da3d87ea364f66a3d173141a0d70bf7626
SpartanSOS <markb539@gmail.com>
2016-05-25 14:44:47 -0500

    add static google map

--- client/components/campgroundMapView.js
+++ client/components/campgroundMapView.js
@@ -1,1 +1,26 @@
 //campground map
+
+class CampgroundMap extends React.Component {
+	render(){
+		return (
+			<div className='campground-map'>
+			<!--CampgroundIcon Component-->
+			<div className='campground-icon'>
+			</div>
+			<!--CampgroundIcon End-->
+			</div>
+		)
+	}
+}
+
+
+
+
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+}
