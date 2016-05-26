0d75d788eddd0e0c1a9e8ca46e5bed4102fd9b90
SpartanSOS <markb539@gmail.com>
2016-05-25 22:04:07 -0500

    google maps changes

--- client/components/campgroundMapView.js
+++ client/components/campgroundMapView.js
@@ -1,5 +1,9 @@
 //campground map
 
+//inherit campgrounds from parent
+//create icons on map
+//render map completely
+
 class CampgroundMap extends React.Component {
 	render(){
 		return (
@@ -14,13 +18,21 @@ class CampgroundMap extends React.Component {
 }
 
 
-
-
 function initMap() {
-  // Create a map object and specify the DOM element for display.
+  // Create a map object and specify the DOM element for display.	
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 30.48276099999998, lng: -97.6564032},
     scrollwheel: false,
     zoom: 11
   });
+  var myLatLng = {lat: 30.48276099999998, lng: -97.6564032};
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
 }
+
+ReactDOM.render(
+	<CampgroundMap />, document.getElementById('map');
+)
