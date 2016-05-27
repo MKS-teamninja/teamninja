ddb29511a9f636499dbe270b974352b0feaea1c5
jackjhall22 <jackjhall22@me.com>
2016-05-27 11:13:15 -0500

    Refactored back to working state, Second ajax function displays  campgrounds to page

---
+++ client/components/initialView.js
@@ -0,0 +1,13 @@
+
+//initial app view
+
+// var React = require('react');
+
+// var HelloMessage = React.createClass({
+//   render: function() {
+//     return <div>Hello {this.props.name}</div>;
+//   }
+// });
+
+// module.exports = HelloMessage;
+
