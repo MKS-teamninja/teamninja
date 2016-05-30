2f30fcdda142acb072f870a525f3840acd300a56
Matt DuBose <matthew.dubose@gmail.com>
2016-05-30 18:39:58 -0500

    clean up

--- client/components/campgroundResultView.js
+++
@@ -1,9 +0,0 @@
-class CampgroundResult extends React.Component({
-    render() {
-      return (
-        <div className="commentList">
-          Hello, world! I am a CommentList.
-        </div>
-      );
-    }
-  });
