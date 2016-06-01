300325d2972b1ea28a2f6ffdc1f8347d6bf6cbf4
Sin Cheung Ko <scko823@gmail.com>
2016-06-01 12:17:32 -0500

    config index.js to export route if it is in testing environment, continue to set up app if in dev and production environment

--- server/index.js
+++ server/index.js
@@ -78,34 +78,32 @@ routes.get('/searchcs', function (req, res) {
   })
 })
 
-//
-// The Catch-all Route
-// This is for supporting browser history pushstate.
-// NOTE: Make sure this route is always LAST.
-//
-routes.get('/*', function(req, res){
-  res.sendFile( assetFolder + '/index.html' )
-})
-
-//
-// We're in development or production mode;
-// create and run a real server.
-//
-var app = express()
-
-// Parse incoming request bodies as JSON
-app.use( require('body-parser').json() )
-
-// Mount our main router
-app.use('/', routes)
-
-// Start the server!
-var port = process.env.PORT || 4000
-app.listen(port)
-console.log("Listening on port", port)
-
-
-//export route for testing
 if (process.env.NODE_ENV = 'test'){
+  //export route for testing
   module.exports = routes
+}else{
+  // in dev and production environment, keep on setting up the app
+  //
+  // The Catch-all Route
+  // This is for supporting browser history pushstate.
+  // NOTE: Make sure this route is always LAST.
+  //
+  routes.get('/*', function(req, res){
+    res.sendFile( assetFolder + '/index.html' )
+  })
+  //
+  // We're in development or production mode;
+  // create and run a real server.
+  //
+  var app = express()
+  // Parse incoming request bodies as JSON
+  app.use( require('body-parser').json() )
+
+  // Mount our main router
+  app.use('/', routes)
+
+  // Start the server!
+  var port = process.env.PORT || 4000
+  app.listen(port)
+  console.log("Listening on port", port)
 }
