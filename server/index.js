9aecb03ffb7fc94aa2bf53f56160623bb96f39cb
Sin Cheung Ko <scko823@gmail.com>
2016-05-31 19:51:39 -0500

    server/index.js is restructured to unify routing in production, development and testing env

--- server/index.js
+++ server/index.js
@@ -41,70 +41,71 @@ var assetFolder = Path.resolve(__dirname, '../client/public')
 routes.use(express.static(assetFolder))
 
 
-if (process.env.NODE_ENV !== 'test') {
-  //
-  // Campground query route
-  // Returns all campgrounds within the given radius
-  // Client should include user's latitude, longitude, and radius (in miles)
-  // as request parameters named lat, lon, rad
-  // Example URL:
-  //   http://localhost:4000/searchcg?lat=30.48276099999998&lon=-97.6564032&rad=30
-  //
-  routes.get('/searchcg', function (req, res) {
-    console.log("Request lat: ", req.query.lat);
-    console.log("Request lon: ", req.query.lon);
-    console.log("Request rad: ", req.query.rad);
-    db.queryCampgrounds()
-      .then(function(cgs) {
-        var filtered = helpers.cgFilter(cgs, req.query.lat, req.query.lon, req.query.rad);
-        // console.log("Filtered campground set: ", filtered);
-        res.status(200).send(filtered);
-    })
+
+//
+// Campground query route
+// Returns all campgrounds within the given radius
+// Client should include user's latitude, longitude, and radius (in miles)
+// as request parameters named lat, lon, rad
+// Example URL:
+//   http://localhost:4000/searchcg?lat=30.48276099999998&lon=-97.6564032&rad=30
+//
+routes.get('/searchcg', function (req, res) {
+  console.log("Request lat: ", req.query.lat);
+  console.log("Request lon: ", req.query.lon);
+  console.log("Request rad: ", req.query.rad);
+  db.queryCampgrounds()
+    .then(function(cgs) {
+      var filtered = helpers.cgFilter(cgs, req.query.lat, req.query.lon, req.query.rad);
+      // console.log("Filtered campground set: ", filtered);
+      res.status(200).send(filtered);
   })
+})
 
-  //
-  // Campsite query route:
-  // Returns all campsites within a given campground
-  // Client should include campground ID as a request parameter named cgId
-  // Example URL:
-  //   http://localhost:4000/searchcs?cgId=820400
-  //
-  routes.get('/searchcs', function (req, res) {
-    console.log("Requested campground ID: ", req.query.cgId);
-    db.queryCampsites(req.query.cgId)
-      .then(function(cs) {
-        // console.log("Campsites in campground #" + req.query.cgId + ": ", cs);
-        res.status(200).send(cs);
-    })
+//
+// Campsite query route:
+// Returns all campsites within a given campground
+// Client should include campground ID as a request parameter named cgId
+// Example URL:
+//   http://localhost:4000/searchcs?cgId=820400
+//
+routes.get('/searchcs', function (req, res) {
+  console.log("Requested campground ID: ", req.query.cgId);
+  db.queryCampsites(req.query.cgId)
+    .then(function(cs) {
+      // console.log("Campsites in campground #" + req.query.cgId + ": ", cs);
+      res.status(200).send(cs);
   })
+})
 
-  //
-  // The Catch-all Route
-  // This is for supporting browser history pushstate.
-  // NOTE: Make sure this route is always LAST.
-  //
-  routes.get('/*', function(req, res){
-    res.sendFile( assetFolder + '/index.html' )
-  })
+//
+// The Catch-all Route
+// This is for supporting browser history pushstate.
+// NOTE: Make sure this route is always LAST.
+//
+routes.get('/*', function(req, res){
+  res.sendFile( assetFolder + '/index.html' )
+})
 
-  //
-  // We're in development or production mode;
-  // create and run a real server.
-  //
-  var app = express()
+//
+// We're in development or production mode;
+// create and run a real server.
+//
+var app = express()
 
-  // Parse incoming request bodies as JSON
-  app.use( require('body-parser').json() )
+// Parse incoming request bodies as JSON
+app.use( require('body-parser').json() )
 
-  // Mount our main router
-  app.use('/', routes)
+// Mount our main router
+app.use('/', routes)
 
-  // Start the server!
-  var port = process.env.PORT || 4000
-  app.listen(port)
-  console.log("Listening on port", port)
-}
-else {
-  // We're in test mode; make this file importable instead.
+// Start the server!
+var port = process.env.PORT || 4000
+app.listen(port)
+console.log("Listening on port", port)
+
+
+//export route for testing
+if (process.env.NODE_ENV = 'test'){
   module.exports = routes
 }
